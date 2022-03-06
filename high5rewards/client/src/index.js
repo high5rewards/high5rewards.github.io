911a90970b8f1e262220d1142d74bdd128816c8f
high5rewards <98888006+high5rewards@users.noreply.github.com>
2022-03-06 14:57:22 -0800

    Update index.js

--- high5rewards/client/src/index.js
+++ high5rewards/client/src/index.js
@@ -7,14 +7,11 @@ import './index.css';
 import {MoralisProvider} from "react-moralis";
 import reportWebVitals from './reportWebVitals';
 
-const appId = "zLRtY4Sr9aU8MwX3YkTsb2g4LepZjIruPWlQdyD3"
-const serverUrl = "https://s5pfoouebnsf.usemoralis.com:2053/server"
-
 
 console.log(appId, serverUrl)
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+    <MoralisProvider appId={process.env.APPID} serverUrl={process.env.SERVERURL}>
       <App />
     </ MoralisProvider >
   </React.StrictMode>,
